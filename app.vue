8c4f6e4fdac954c7595634bd9e08cd13c79af76b
oleg-lekhnitsky <oleg.lekhnitsky@gmail.com>
2023-04-14 00:02:13 +0300

    metr

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    
+
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
@@ -10,10 +10,6 @@
 
 
 <style>
-
-*{
-
-}
 
 html,
 body {
@@ -25,6 +21,7 @@ body {
   font-weight: 400;
   /* transition: background 1s ease, color 1.3s ease; */
 }
+
 /* .dark-mode body {
   background-color: #091a28;
   color: #ebf4f1;
@@ -32,32 +29,14 @@ body {
 
 .page-enter-active,
 .page-leave-active {
-  transition: all .5s cubic-bezier(.68,.07,.68,.97);
+  transition: all .5s cubic-bezier(.68, .07, .68, .97);
 }
+
 .page-enter-from,
 .page-leave-to {
   transform: translateY(70px);
   opacity: 0;
   /* filter: blur(1rem); */
-  
+
 }
-
-
 </style>
-<!-- Yandex.Metrika counter -->
-<script type="text/javascript" >
-   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-   m[i].l=1*new Date();
-   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-   ym(93212273, "init", {
-        clickmap:true,
-        trackLinks:true,
-        accurateTrackBounce:true,
-        webvisor:true
-   });
-</script>
-<noscript><div><img src="https://mc.yandex.ru/watch/93212273" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-<!-- /Yandex.Metrika counter -->
