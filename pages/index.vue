1ba8f78326bda3e8c013ce9994b8d9309f31d487
oleg-lekhnitsky <oleg.lekhnitsky@gmail.com>
2023-05-21 20:35:14 +0300

    video

--- pages/index.vue
+++ pages/index.vue
@@ -1,38 +1,35 @@
 
 <template>
+    <div class="wrapper desktop">
+        <next style="position: absolute; right: 2vw; top: 1vw;" />
+        <h1 class="headline">(intro)</h1>
+
+        <h1>I design digital solutions for businesses combining my expertise in UX/UI design, front-end development and
+            branding.
+            <span style="color: var(--text-light);">I am passionate about creating user-centered products that solve
+                problems and make life a little better. </span>
+
+        </h1>
+        <p style=" font-size: large; letter-spacing: .03em ;margin-bottom: 2em;">Snippets from my previous projects. These
+            are designed to leave you in awe and make you want to explore more, rather than leaving the website right away.
+        </p>
+        <div class="content-wrapper">
 
 
-
-        
-        <div class="wrapper desktop">
-            <next style="position: absolute; right: 2vw; top: 1vw;"/>
-            <h1 class="headline">(intro)</h1>
-
-            <h1>I design digital solutions for businesses combining my expertise in UX/UI design, front-end development and
-                branding.
-                <span style="color: var(--text-light);">I am passionate about creating user-centered products that solve
-                    problems and make life a little better. </span>
-
-            </h1>
-            <p style=" font-size: large; letter-spacing: .03em ;margin-bottom: 2em;">Snippets from my previous projects. These are designed to leave you in awe and make you want to explore more, rather than leaving the website right away.</p>
-            <div class="content-wrapper">
+            <video src="/sh.mp4"
+                style="width: 100%;  height: auto; background-color: var(--bg); border-radius: 1vw; aspect-ratio: 1.54700/1; clip-path: inset(1px 1px);"
+                autoplay muted loop playsinline></video>
 
 
-                    <video  src="/sh.mp4" style="width: 100%;  height: auto; background-color: var(--bg); border-radius: 1vw; aspect-ratio: 1.54700/1; clip-path: inset(1px 1px);" autoplay muted loop></video>
-        
-                
-
-            </div>
-            
-            <NuxtLink to="/contact" class="cta-hero">Contact</NuxtLink>
 
         </div>
 
+        <NuxtLink to="/contact" class="cta-hero">Contact</NuxtLink>
+
+    </div>
 </template>
   
-<style>
-
-</style>
+<style></style>
 
 
 <style>
